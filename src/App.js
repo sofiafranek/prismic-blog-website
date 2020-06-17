87af589060f80f7cb4e3d8d28c62d9160f550f4b
Sofia Franek <sofiafranek@icloud.com>
2020-06-17 14:39:38 +0100

    Removed editing button.

--- src/App.js
+++ src/App.js
@@ -1,32 +1,27 @@
 import React, { Fragment } from 'react';
-import { Helmet } from 'react-helmet';
-import {
-  BrowserRouter,
-  Route,
-  Switch,
-  Redirect
-} from 'react-router-dom';
+// import { Helmet } from 'react-helmet';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import { BlogHome, NotFound, Post, Preview } from './pages';
-import { apiEndpoint } from './prismic-configuration';
+// import { apiEndpoint } from './prismic-configuration';
 
 /**
  * Main app component
  */
 const App = () => {
-  const repoNameArray = /([^/]+)\.cdn.prismic\.io\/api/.exec(apiEndpoint);
-  const repoName = repoNameArray[1];
+  // const repoNameArray = /([^/]+)\.cdn.prismic\.io\/api/.exec(apiEndpoint);
+  // const repoName = repoNameArray[1];
 
   return (
     <Fragment>
-      <Helmet>
+      {/* <Helmet>
         <script async defer src={`//static.cdn.prismic.io/prismic.js?repo=${repoName}&new=true`} />
-      </Helmet>
+      </Helmet> */}
       <BrowserRouter>
         <Switch>
-          <Redirect exact from='/blog/' to='/' />
-          <Route exact path='/' component={BlogHome} />
-          <Route exact path='/preview' component={Preview} />
-          <Route exact path='/blog/:uid' component={Post} />
+          <Redirect exact from="/blog/" to="/" />
+          <Route exact path="/" component={BlogHome} />
+          <Route exact path="/preview" component={Preview} />
+          <Route exact path="/blog/:uid" component={Post} />
           <Route component={NotFound} />
         </Switch>
       </BrowserRouter>
